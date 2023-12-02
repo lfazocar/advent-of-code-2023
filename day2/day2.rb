5893085770ec9f9ec4ab97ea1706896e181ee2e0
Felipe <lfazocar@users.noreply.github.com>
2023-12-02 16:19:34 -0300

    Day 2 - Cube Conundrum (Part 2)

--- day2/day2.rb
+++ day2/day2.rb
@@ -36,3 +36,31 @@ input.each do |line|
 end
 
 puts "Part 1 solution is: #{sum}"
+
+# Solve puzzle - part 2
+
+# The power of a set of cubes is equal to the numbers of red, green, and blue
+# cubes multiplied together. For each game, find the minimum set of cubes that
+# must have been present. What is the sum of the power of these sets?
+
+sum = 0
+input.each do |line|
+  game = line.split
+  min_red, min_green, min_blue = 0, 0, 0
+  (2...game.length).step(2).each do |i|
+    cube_amount = game[i].to_i
+    color = game[i+1]
+    case color
+    when 'red'
+      min_red = cube_amount if cube_amount > min_red
+    when 'green'
+      min_green = cube_amount if cube_amount > min_green
+    when 'blue'
+      min_blue = cube_amount if cube_amount > min_blue
+    end
+  end
+  power = min_red * min_green * min_blue
+  sum += power
+end
+
+puts "Part 2 solution is: #{sum}"
