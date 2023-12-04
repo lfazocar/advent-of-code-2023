77835bda93d9b8b2833487e543cf6845e28dad8f
Felipe <lfazocar@users.noreply.github.com>
2023-12-04 20:40:50 -0300

    Day 4 - Scratchcards (Part 2)

--- day4/day4.rb
+++ day4/day4.rb
@@ -25,3 +25,34 @@ input.each do |line|
 end
 
 puts "Part 1 solution is: #{score}"
+
+# There's no such thing as "points". Instead, scratchcards only cause you to
+# win more scratchcards equal to the number of winning numbers you have.
+# Specifically, you win copies of the scratchcards below the winning card equal
+# to the number of matches. So, if card 10 were to have 5 matching numbers,
+# you would win one copy each of cards 11, 12, 13, 14, and 15.
+# Including the original set of scratchcards, how many total scratchcards do
+# you end up with?
+
+# Solve puzzle - part 2
+
+scratchcards = Array.new(input.length, 1)
+input.each_with_index do |line, card_n|
+  scratchcards[card_n].times do
+    card = line.split('|')
+    winners = card[0].split
+    numbers = card[1].split
+
+    matches = 0
+    numbers.each do |num|
+      if winners.include?(num)
+        matches += 1
+        scratchcards[card_n + matches] += 1
+      end
+    end
+    break if matches == 0
+
+  end
+end
+
+puts "Part 2 solution is: #{scratchcards.sum}"
