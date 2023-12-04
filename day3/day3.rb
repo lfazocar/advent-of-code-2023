899bb708a221ace9d360300a626baac6ec1f1eac
Felipe <lfazocar@users.noreply.github.com>
2023-12-03 21:43:53 -0300

    Day 3 - Gear Ratios (Part 2)

--- day3/day3.rb
+++ day3/day3.rb
@@ -11,10 +11,10 @@ input = File.read(dir).lines
 
 # Valid positions function
 
-def valid_positions(symbol_pos)
+def adjacent_positions(symbols)
   positions = []
-  line = symbol_pos[0]
-  index = symbol_pos[1]
+  line = symbols[0]
+  index = symbols[1]
   1.downto(-1) do |x|
     1.downto(-1) do |y|
       next if x == 0 && y == 0
@@ -28,7 +28,8 @@ end
 
 # Find symbols and numbers
 
-symbols_pos = [] # [[line, index]]
+symbols = [] # [[line, index]]
+gears = [] # [[line, index]]
 numbers = []  # [number, [[line, index]]]
 (0...input.length).each do |line_i|
   num = ''
@@ -44,7 +45,8 @@ numbers = []  # [number, [[line, index]]]
         numbers.push([num.to_i, num_pos])
         num = ''
       end
-      symbols_pos.push([line_i, char_i]) if char != '.' && char != "\n"
+      gears.push([line_i, char_i]) if char == '*'
+      symbols.push([line_i, char_i]) if char != '.' && char != "\n"
     end
   end
 end
@@ -52,19 +54,57 @@ end
 # Solve puzzle - part 1
 
 valid_pos = []
-symbols_pos.each do |pos|
-  valid_pos += valid_positions(pos)
+symbols.each do |pos|
+  valid_pos += adjacent_positions(pos)
 end
 
-sum = 0
+part_sum = 0
 numbers.each do |number|
   value = number[0]
   number[1].each do |pos|
     if valid_pos.include?(pos)
-      sum += value
+      part_sum += value
       break
     end
   end
 end
 
-puts "Part 1 solution is: #{sum}"
+puts "Part 1 solution is: #{part_sum}"
+
+# A gear is any * symbol that is adjacent to exactly two part numbers. Its gear
+# ratio is the result of multiplying those two numbers together. This time, you
+# need to find the gear ratio of every gear and add them all up so that the
+# engineer can figure out which gear needs to be replaced.
+
+# Solve puzzle - Part 2
+
+gear_sum = 0
+gears.each do |gear|
+  ratio_pos = adjacent_positions(gear)
+
+  ratio, first, first_pos = 0, 0, []
+  ratio_pos.each do |pos|
+
+    numbers.each do |number|
+    # [0] = value, [1] = positions array
+      if number[1].include?(pos) && number[1] != first_pos
+        if first == 0
+          first = number[0]
+          first_pos = number[1]
+        else
+          ratio += first * number[0]
+          break
+        end
+      end
+    end
+
+    if ratio != 0
+      gear_sum += ratio
+      ratio = 0
+      break
+    end
+
+  end
+end
+
+puts "Part 2 solution is: #{gear_sum}"
