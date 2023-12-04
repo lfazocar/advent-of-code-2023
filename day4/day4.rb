c4464caa81f173769298fbb8ef8b7aebd0a56bea
Felipe <lfazocar@users.noreply.github.com>
2023-12-04 20:52:57 -0300

    Refactored day 4 puzzle

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,3 +1,4 @@
+# Part 1
 # Picking one up, it looks like each card has two lists of numbers separated
 # by a vertical bar (|): a list of winning numbers and then a list of numbers
 # you have. You organize the information into a table (your puzzle input).
@@ -5,27 +6,7 @@
 # first doubles the point value of that card.
 # How many points are they worth in total?
 
-# Read input
-
-dir = File.join(File.dirname(__FILE__),'input.txt')
-input = File.read(dir).lines.map { |line| line[10..-2] }
-
-# Solve puzzle - part 1
-
-score = 0
-input.each do |line|
-  card = line.split('|')
-  winners = card[0].split
-  numbers = card[1].split
-  matches = 0
-  numbers.each do |num|
-    matches += 1 if winners.include?(num)
-  end
-  score += 2 ** (matches - 1) if matches > 0
-end
-
-puts "Part 1 solution is: #{score}"
-
+# Part 2
 # There's no such thing as "points". Instead, scratchcards only cause you to
 # win more scratchcards equal to the number of winning numbers you have.
 # Specifically, you win copies of the scratchcards below the winning card equal
@@ -34,25 +15,28 @@ puts "Part 1 solution is: #{score}"
 # Including the original set of scratchcards, how many total scratchcards do
 # you end up with?
 
-# Solve puzzle - part 2
+# Read input
 
+dir = File.join(File.dirname(__FILE__),'input.txt')
+input = File.read(dir).lines.map { |line| line[10..-2] }
+
+# Solve puzzle
+
+score = 0
 scratchcards = Array.new(input.length, 1)
 input.each_with_index do |line, card_n|
+  card = line.split('|')
+  winners, numbers = card[0].split, card[1].split
+
+  matches = 0
+  numbers.each { |num| matches += 1 if winners.include?(num) }
+  score += 2 ** (matches - 1) if matches > 0
+
   scratchcards[card_n].times do
-    card = line.split('|')
-    winners = card[0].split
-    numbers = card[1].split
-
-    matches = 0
-    numbers.each do |num|
-      if winners.include?(num)
-        matches += 1
-        scratchcards[card_n + matches] += 1
-      end
-    end
-    break if matches == 0
-
+    1.upto(matches) { |offset| scratchcards[card_n + offset] += 1 }
   end
+
 end
 
+puts "Part 1 solution is: #{score}"
 puts "Part 2 solution is: #{scratchcards.sum}"
