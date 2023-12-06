df8e0e2921b0693f3df0553fa2f390d772d03e34
Felipe <lfazocar@users.noreply.github.com>
2023-12-06 15:58:50 -0300

    Day 5 - If You Give A Seed A Fertilizer (Part 2)

--- day5/day5.rb
+++ day5/day5.rb
@@ -6,34 +6,58 @@ input = File.read(dir).delete("^:1234567890 \n")[1..-1].split(':')
 seeds = input.shift.split.map(&:to_i)
 
 map_list = input.map { |text|
-  text[1..-2].split("\n").map{ |line| line.split.map(&:to_i) }
+  text[1...-1].split("\n").map{ |line| line.split.map(&:to_i) }
 }
 
-# Map destination function
+# Solve puzzle
 
-def map_destination(source, map)
-  destination = source
-  map.each do |map_array|
-    source_range = (map_array[1]...map_array[1] + map_array[2])
-    if source_range.include?(source)
-      destination = map_array[0] + source - map_array[1]
+# Convert source to destination function
+# Destination range upper limit is required for part 2
+
+def map_destination(source, map, limit = false)
+  destination = [source] # [source, upper limit]
+
+  map.each do |map_data|
+    destination_start = map_data[0]
+    source_start = map_data[1]
+    range = map_data[2]
+
+    if source.between?(source_start, source_start + range)
+      destination[0] += destination_start - source_start
+
+      if limit
+        if destination_start - source_start > 0
+          destination.push(destination_start + range)
+        end
+
+      end
+
       break
     end
+
   end
   destination
 end
 
-# Solve puzzle - part 1
+# Iterate recursively through the maps
 
-def iterate_maps(seed_array, map_list)
+def iterate_maps(seed_array, map_list, limit = false)
   return seed_array if map_list.empty?
+  mapped = []
 
   map = map_list.shift
-  seed_array.map! { |seed| map_destination(seed, map) }
+  seed_array.each do |seed|
+    mapped += map_destination(seed, map, limit)
+  end
 
-  iterate_maps(seed_array, map_list)
+  iterate_maps(mapped.uniq, map_list, limit)
 end
 
-iterate_maps(seeds, map_list)
+puts "Part 1 solution is: #{iterate_maps(seeds.dup, map_list.dup).min}"
 
-puts "Part 1 solution is: #{seeds.min}"
+seed_ranges = []
+0.step(seeds.length-1, 2) { # Only need start and end of seed range
+  |i| seed_ranges.push(seeds[i], seeds[i] + seeds[i+1] - 1)
+}
+
+puts "Part 2 solution is: #{iterate_maps(seed_ranges, map_list, true).min}"
